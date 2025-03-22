18351189fe106e05d3cc034adfdc94ff76d56021
Mina Medhat <minamedhat123456@gmail.com>
2025-03-22 14:44:15 +0200

    Add Test Cases

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FizzBuzzDetector detector = new FizzBuzzDetector();
+            string input = """
+                        Mary had a little lamb
+                        Little lamb, little lamb
+                        Mary had a little lamb
+                        It's fleece was white as snow
+                        """;
+            FizzBuzzResult result = detector.GetOverlappings(input);
+
+            Console.WriteLine($"Output String:\n{result.Output}\n\nCount: {result.Count}");
         }
     }
 }
