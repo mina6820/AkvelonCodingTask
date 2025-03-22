e154ee58bc1fdac5f9303fa807bb150343120ae7
Mina Medhat <minamedhat123456@gmail.com>
2025-03-22 15:25:32 +0200

    space

--- FizzBuzzDetector.cs
+++ FizzBuzzDetector.cs
@@ -25,13 +25,13 @@ namespace AkvelonCodingTask
         /// <exception cref="ArgumentException">
         /// Thrown when the input string is null, or its length is less than 7 or greater than 100.
         /// </exception>
-        public FizzBuzzResult GetOverlappings(string input){
+        public FizzBuzzResult GetOverlappings(string input) {
             if (input == null){
                 throw new ArgumentException("Input string cannot be null.");
             }
 
             input = input.Replace("\r\n", "\n");
-            if (input.Length < 7 || input.Length > 100){
+            if (input.Length < 7 || input.Length > 100) {
                 throw new ArgumentException("Input string length must be between 7 and 100 characters.");
             }
 
@@ -41,8 +41,8 @@ namespace AkvelonCodingTask
             int replacementCount = 0;
             var outputBuilder = new StringBuilder();
 
-            foreach (string token in tokens){
-                if (string.IsNullOrEmpty(token) || string.IsNullOrWhiteSpace(token)){
+            foreach (string token in tokens) {
+                if (string.IsNullOrEmpty(token) || string.IsNullOrWhiteSpace(token)) {
                     outputBuilder.Append(token);
                     continue;
                 }
@@ -50,28 +50,28 @@ namespace AkvelonCodingTask
                 // Remove punctuation from the beginning and end to obtain the core word.
                 int start = 0;
                 int end = token.Length;
-                while (start < end && char.IsPunctuation(token[start])){
+                while (start < end && char.IsPunctuation(token[start])) {
                     start++;
                 }
-                while (end > start && char.IsPunctuation(token[end - 1])){
+                while (end > start && char.IsPunctuation(token[end - 1])) {
                     end--;
                 }
                 string coreWord = token.Substring(start, end - start);
 
-                if (!string.IsNullOrEmpty(coreWord) && IsAlphanumeric(coreWord)){
+                if (!string.IsNullOrEmpty(coreWord) && IsAlphanumeric(coreWord)) {
                     wordCount++;
                     string replacement = null;
-                    if (wordCount % 15 == 0){
+                    if (wordCount % 15 == 0) {
                         replacement = "FizzBuzz";
                     }
-                    else if (wordCount % 3 == 0){
+                    else if (wordCount % 3 == 0) {
                         replacement = "Fizz";
-                    }
-                    else if (wordCount % 5 == 0){
+                    } 
+                    else if (wordCount % 5 == 0) {
                         replacement = "Buzz";
                     }
 
-                    if (replacement != null){
+                    if (replacement != null) {
                         outputBuilder.Append(replacement);
                         replacementCount++;
                         continue;
@@ -81,7 +81,7 @@ namespace AkvelonCodingTask
                 outputBuilder.Append(token);
             }
 
-            return new FizzBuzzResult{
+            return new FizzBuzzResult {
                 Output = outputBuilder.ToString(),
                 Count = replacementCount
             };
@@ -94,9 +94,9 @@ namespace AkvelonCodingTask
         /// <returns>
         /// True if all characters in the string are letters or digits; otherwise, false.
         /// </returns>
-        private bool IsAlphanumeric(string s){
-            foreach (char c in s){
-                if (!(char.IsLetterOrDigit(c) || c == '\'')){
+        private bool IsAlphanumeric(string s) {
+            foreach (char c in s) {
+                if (!(char.IsLetterOrDigit(c) || c == '\'')) {
                     return false;
                 }
             }
